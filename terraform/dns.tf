c44035cd6f4bdc93b08d8d81cbf44ac682af5b87
Arba Sasmoyo <arba.sasmoyo@gmail.com>
2020-07-04 14:51:12 +0700

    point staging domains to internal ip

--- terraform/dns.tf
+++ terraform/dns.tf
@@ -1,5 +1,6 @@
 locals {
     ingress_ips = ["51.79.142.59", "51.79.140.121", "51.79.141.163"]
+    internal_ingress_ips = ["10.99.1.85", "10.99.2.245", "10.99.0.177"]
     cname_domains = ["api", "console", "docker", "*.apps"]
     staging_domain_cnames = ["api", "docker", "*.apps"]
     staging_domain_local = ["staging", "console.staging"]
@@ -48,24 +49,11 @@ resource "google_dns_record_set" "internal_awanku_id" {
   ttl  = 300
 
   managed_zone = data.google_dns_managed_zone.awanku_id.name
-  rrdatas = [
-    "10.99.1.85",
-    "10.99.2.245",
-    "10.99.0.177",
-  ]
+  rrdatas = local.internal_ingress_ips
 }
 
 data "google_dns_managed_zone" "awanku_xyz" {
   name = "awanku-xyz"
-}
-
-resource "google_dns_record_set" "awanku_xyz" {
-  name = data.google_dns_managed_zone.awanku_xyz.dns_name
-  type = "A"
-  ttl  = 300
-
-  managed_zone = data.google_dns_managed_zone.awanku_xyz.name
-  rrdatas = local.ingress_ips
 }
 
 resource "google_dns_record_set" "staging_local_awanku_xyz" {
@@ -79,15 +67,13 @@ resource "google_dns_record_set" "staging_local_awanku_xyz" {
   rrdatas = ["127.0.0.1"]
 }
 
-resource "google_dns_record_set" "staging_cname_awanku_xyz" {
+resource "google_dns_record_set" "staging_internal_awanku_xyz" {
   for_each = toset(local.staging_domain_cnames)
 
   name = "${each.value}.staging.${data.google_dns_managed_zone.awanku_xyz.dns_name}"
-  type = "CNAME"
+  type = "A"
   ttl  = 300
 
   managed_zone = data.google_dns_managed_zone.awanku_xyz.name
-  rrdatas = [
-    google_dns_record_set.awanku_xyz.name,
-  ]
+  rrdatas = local.internal_ingress_ips
 }
